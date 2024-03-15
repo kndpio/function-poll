49b458711e8c64e0d944de70af707a822602f898
Catalin <catalincretu1234@gmail.com>
2024-03-15 15:32:28 +0200

    Updated function to keep XR not synced while voting

--- fn.go
+++ fn.go
@@ -9,9 +9,11 @@ import (
 	"time"
 
 	"github.com/slack-go/slack"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 
+	v1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 	"github.com/crossplane/function-sdk-go/logging"
 	fnv1beta1 "github.com/crossplane/function-sdk-go/proto/v1beta1"
 	"github.com/crossplane/function-sdk-go/request"
@@ -38,6 +40,15 @@ type Poll struct {
 type Voter struct {
 	Name   string `json:"name"`
 	Status string `json:"status"`
+}
+
+type initialResources struct {
+	Input            *v1beta1.Input
+	API              *slack.Client
+	XR               *resource.Composite
+	Rsp              *fnv1beta1.RunFunctionResponse
+	CurrentTimestamp int
+	Desired          map[resource.Name]*resource.DesiredComposed
 }
 
 // Function returns whatever response you ask it to.
@@ -197,8 +208,32 @@ func (f *Function) transformK8sResource(input *v1beta1.Input) composed.Unstructu
 	return unstructuredData
 }
 
-// Check if user is not a bot
-func realUsers(members []string, api *slack.Client) []string {
+// Get initial resources
+func getInitialResources(req *fnv1beta1.RunFunctionRequest) (*initialResources, error) {
+	input := &v1beta1.Input{}
+	if err := request.GetInput(req, input); err != nil {
+		fmt.Println(err)
+	}
+	desired, err := request.GetDesiredComposedResources(req)
+	if err != nil {
+		return nil, err
+	}
+	api := slack.New(input.SlackAPIToken)
+	rsp := response.To(req, response.DefaultTTL)
+
+	xr, err := request.GetObservedCompositeResource(req)
+	if err != nil {
+		return nil, err
+	}
+	return &initialResources{Input: input, API: api, XR: xr, Rsp: rsp, Desired: desired, CurrentTimestamp: int(time.Now().Unix())}, nil
+}
+
+// Get and process slack members
+func processSlackMembers(api *slack.Client, channelID string) ([]string, error) {
+	members, _, err := api.GetUsersInConversation(&slack.GetUsersInConversationParameters{ChannelID: channelID})
+	if err != nil {
+		return nil, err
+	}
 	realUsers := make([]string, 0)
 	for _, memberID := range members {
 		userInfo, err := api.GetUserInfo(memberID)
@@ -211,55 +246,59 @@ func realUsers(members []string, api *slack.Client) []string {
 			realUsers = append(realUsers, userInfo.Name)
 		}
 	}
-	return realUsers
+	return realUsers, nil
+}
+
+// Check if the dueOrderTime is passed or all users have voted
+func checkDueOrderTimeAndVoteCount(xr *resource.Composite, currentTimestamp int, users []string) bool {
+	dueOrderTimeString, _ := xr.Resource.GetString("spec.dueOrderTime")
+	dueOrderTime, _ := strconv.Atoi(dueOrderTimeString)
+	voters, _ := xr.Resource.GetStringArray("spec.voters")
+	fmt.Println("DueOrderTime: ", dueOrderTime, "CurrentTimestamp: ", currentTimestamp)
+	return currentTimestamp >= dueOrderTime || len(voters) == len(users)
 }
 
 // RunFunction adds a Deployment and the new object template to the desired state.
 func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequest) (*fnv1beta1.RunFunctionResponse, error) {
 
-	input := &v1beta1.Input{}
-	if err := request.GetInput(req, input); err != nil {
-		fmt.Println(err)
-	}
-
-	api := slack.New(input.SlackAPIToken)
-	rsp := response.To(req, response.DefaultTTL)
-	xr, err := request.GetObservedCompositeResource(req)
+	initialResources, err := getInitialResources(req)
 	if err != nil {
-		return rsp, err
+		return initialResources.Rsp, err
 	}
 
-	members, _, err := api.GetUsersInConversation(&slack.GetUsersInConversationParameters{
-		ChannelID: input.SlackChanelID,
-	})
+	var conditionStatus corev1.ConditionStatus
+
+	users, err := processSlackMembers(initialResources.API, initialResources.Input.SlackChanelID)
 	if err != nil {
 		fmt.Printf("Error getting conversation members: %v", err)
 	}
-	users := realUsers(members, api)
-	Voters, _ := xr.Resource.GetStringArray("spec.voters")
-	dueOrderTimeString, _ := xr.Resource.GetString("spec.dueOrderTime")
-	dueOrderTime, _ := strconv.Atoi(dueOrderTimeString)
-	currentTimestamp := int(time.Now().Unix())
-	fmt.Println("DueOrderTime: ", dueOrderTime, "CurrentTimestamp: ", currentTimestamp)
 
-	if currentTimestamp >= dueOrderTime || len(Voters) == len(users) {
+	if checkDueOrderTimeAndVoteCount(initialResources.XR, initialResources.CurrentTimestamp, users) {
+		conditionStatus = corev1.ConditionTrue
 		fmt.Println("DueOrderTime is passed due, send results in slack chanel")
-	} else {
-		if timeElapsed(xr, rsp) {
-			sendSlackMessage(xr, api, input.SlackChanelID, input.SlackNotifyMessage)
-		}
-
-		desired, err := request.GetDesiredComposedResources(req)
+		initialResources.XR.Resource.SetConditions(v1.Condition{Type: v1.TypeSynced, Status: corev1.ConditionTrue})
+		err = response.SetDesiredCompositeResource(initialResources.Rsp, initialResources.XR)
 		if err != nil {
-			return rsp, err
+			return initialResources.Rsp, err
 		}
 
-		deployment := f.transformK8sResource(input)
-		desired[resource.Name(deployment.GetName())] = &resource.DesiredComposed{Resource: &deployment}
+	} else {
+		conditionStatus = corev1.ConditionFalse
+		if timeElapsed(initialResources.XR, initialResources.Rsp) {
+			sendSlackMessage(initialResources.XR, initialResources.API, initialResources.Input.SlackChanelID, initialResources.Input.SlackNotifyMessage)
+		}
 
-		if err := response.SetDesiredComposedResources(rsp, desired); err != nil {
-			return rsp, err
+		deployment := f.transformK8sResource(initialResources.Input)
+		initialResources.Desired[resource.Name(deployment.GetName())] = &resource.DesiredComposed{Resource: &deployment}
+
+		if err := response.SetDesiredComposedResources(initialResources.Rsp, initialResources.Desired); err != nil {
+			return initialResources.Rsp, err
 		}
 	}
-	return rsp, nil
+	initialResources.XR.Resource.SetConditions(v1.Condition{Type: v1.TypeSynced, Status: conditionStatus})
+	err = response.SetDesiredCompositeResource(initialResources.Rsp, initialResources.XR)
+	if err != nil {
+		return initialResources.Rsp, err
+	}
+	return initialResources.Rsp, nil
 }
